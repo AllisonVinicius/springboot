b50cfd04e7c46cb68028da9967dfc1ec725f36db
allisonvinicius <allisonvinicius63@gmail.com>
2022-02-11 23:16:52 -0400

    dependecy psql and boot-jpa

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,18 +1,14 @@
 package br.com.springboot.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import org.apache.el.stream.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.springboot.model.User;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,7 +16,7 @@ public class UserController {
     private List<User> users = new ArrayList<>();
 
     @GetMapping("/{id}")
-    public User user(@PathVariable("id") Long id) {
+    public Object user(@PathVariable("id") Long id) {
 
         Optional<User> userFind = users.stream().filter(user -> user.getId() == id).findFirst();
 
